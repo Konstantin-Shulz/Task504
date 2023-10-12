fe8a20cb2c0dd237322dc317c523f29e260c887c
Konstantin S <skg@genialconsult.ru>
2023-10-12 21:11:39 +0300

    упростил формулу

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -6,15 +5,14 @@ import java.nio.file.Path;
 import java.util.Scanner;
 
 public class Main {
-    public static String main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         String inputFName = "INPUT.TXT";
         Scanner scanner = new Scanner(new File(inputFName));
         int k = scanner.nextInt();
         String pos;
-        String pos1 = "VGC"; System.out.println(pos1);
-        String pos2 = "CVG"; System.out.println(pos2);
-        String pos3 = "GCV"; System.out.println(pos3);
-
+        String pos1 = "VGC";
+        String pos2 = "CVG";
+        String pos3 = "GCV";
 
             if (k % 3 == 0) {
                 pos = pos3;
@@ -26,12 +24,9 @@ public class Main {
                else pos = pos1;
             }
 
-
-        String OrderFlowers = pos;
         String outputFName = "OUTPUT.TXT";
-
-       String s = String.valueOf(OrderFlowers);
-       Files.writeString(Path.of(outputFName), s);
+        Files.writeString(Path.of(outputFName), pos);
 
     }
+
 }
