3b41e27ca05c8dc03f4e4ba8fd0d0c2412def5b0
Konstantin S <skg@genialconsult.ru>
2023-10-12 20:59:17 +0300

    переделал

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,37 @@
+import javax.swing.*;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static String main(String[] args) throws IOException {
         String inputFName = "INPUT.TXT";
         Scanner scanner = new Scanner(new File(inputFName));
         int k = scanner.nextInt();
-        char g = ('G');
-        char c = ('C');
-        char v = ('V');
-
-        for (int i = 0; i < k; i++) {
-            String OrderFlowers = String.valueOf(g+c+v);
-        }
+        String pos;
+        String pos1 = "VGC"; System.out.println(pos1);
+        String pos2 = "CVG"; System.out.println(pos2);
+        String pos3 = "GCV"; System.out.println(pos3);
 
 
+            if (k % 3 == 0) {
+                pos = pos3;
+            }
+           else {
+               if (k % 3 == 2){
+                   pos = pos2;
+               }
+               else pos = pos1;
+            }
 
-        String OrderFlowers;
-        OrderFlowers = "VGC";
+
+        String OrderFlowers = pos;
         String outputFName = "OUTPUT.TXT";
 
        String s = String.valueOf(OrderFlowers);
        Files.writeString(Path.of(outputFName), s);
+
     }
 }
